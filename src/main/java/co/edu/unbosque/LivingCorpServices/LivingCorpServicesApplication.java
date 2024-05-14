e143be2786d34bd17cbf8a44a050d530f31f5024
MelannyJutinico <melannyjutinico@gmail.com>
2024-05-13 20:32:43 -0500

    Proyecto inicializado

---
+++ src/main/java/co/edu/unbosque/LivingCorpServices/LivingCorpServicesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.LivingCorpServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingCorpServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingCorpServicesApplication.class, args);
+	}
+
+}
